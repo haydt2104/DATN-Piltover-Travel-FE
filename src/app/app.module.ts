5e5fe5c80b4c3b81ce9398e6dbf858461f6aa645
WeirdoJackDX <lhtho1803@gmail.com>
2023-09-08 15:18:02 +0700

    Base-Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,51 @@
+import {
+  CommonModule, LocationStrategy,
+  PathLocationStrategy
+} from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FullComponent } from './admin/layouts/full/full.component';
+
+
+import { NavigationComponent } from './admin/shared/header/navigation.component';
+import { SidebarComponent } from './admin/shared/sidebar/sidebar.component';
+
+import { SpinnerComponent } from './admin/shared/spinner.component';
+import { Approutes } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    SpinnerComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
+    RouterModule.forRoot(Approutes, { useHash: false }),
+    FullComponent,
+    SidebarComponent,
+    NavigationComponent,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
