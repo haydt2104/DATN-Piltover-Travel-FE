fcea8cd5ecad59aae3b25da512d2ecabd0118844
tronghenry273 <101923224+QuiBuuIzTheBest@users.noreply.github.com>
2023-09-15 14:43:48 +0700

    Revert "update"
    This reverts commit 0fd6e73944d2f55eaaaadcff8f90f0128e0e19dc.

--- src/app/admin/dashboard/dashboard-components/sales-ratio/sales-ratio.component.ts
+++ src/app/admin/dashboard/dashboard-components/sales-ratio/sales-ratio.component.ts
@@ -10,7 +10,7 @@ import {
   ApexTooltip,
   ApexTheme,
   ApexGrid,
-  ApexPlotOptions,
+  ApexPlotOptions
 } from 'ng-apexcharts';
 
 export type salesChartOptions = {
@@ -31,74 +31,73 @@ export type salesChartOptions = {
 
 @Component({
   selector: 'app-sales-ratio',
-  templateUrl: './sales-ratio.component.html',
+  templateUrl: './sales-ratio.component.html'
 })
 export class SalesRatioComponent implements OnInit {
-  @ViewChild('chart') chart: ChartComponent = Object.create(null);
+
+  @ViewChild("chart") chart: ChartComponent = Object.create(null);
   public salesChartOptions: Partial<salesChartOptions>;
   constructor() {
     this.salesChartOptions = {
       series: [
         {
-          name: 'Tour',
-          data: [1000, 31, 40, 28, 51, 42, 109, 100, 105, 111, 150, 1000],
+          name: "Tour",
+          data: [0, 31, 40, 28, 51, 42, 109, 100, 105, 111, 150, 300],
         },
         {
-          name: 'Khách Sạn',
+          name: "Khách Sạn",
           data: [0, 11, 32, 45, 32, 34, 52, 41, 55, 81, 100, 150],
-        },
+        }, 
         {
-          name: 'Máy Bay',
+          name: "Máy Bay",
           data: [0, 71, 75, 94, 102, 134, 152, 141, 125, 181, 220, 140],
-        },
-        {
-          name: 'Test',
-          data: [0, 71, 75, 94, 102, 134, 152, 141, 125, 181, 220, 1340],
-        },
+        }
       ],
       chart: {
         fontFamily: 'Rubik,sans-serif',
         height: 390,
         type: 'area',
         toolbar: {
-          show: false,
+          show: false
         },
       },
       dataLabels: {
-        enabled: false,
+        enabled: false
       },
-      colors: ['red', 'yellow', 'blue', 'black'],
+      colors: ["red", "yellow", "blue"],
       stroke: {
-        curve: 'smooth',
+        curve: "smooth",
         width: 1,
       },
       grid: {
         strokeDashArray: 3,
       },
       markers: {
-        size: 3,
+        size: 3
       },
       xaxis: {
         categories: [
-          'Jan',
-          'Feb',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec',
+          "Jan",
+          "Feb",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec"
         ],
       },
       tooltip: {
-        theme: 'dark',
-      },
+        theme: 'dark'
+      }
     };
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
 }
