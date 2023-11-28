6e0aeb30130070f37f9f87ab60d9f12d873cfba4
tronghenry273 <101923224+QuiBuuIzTheBest@users.noreply.github.com>
2023-11-28 19:48:15 +0700

    eviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,23 +1,23 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/',
-  // firebaseConfig: {
-  //   apiKey: "AIzaSyBL8oFvS6G9d2dHC-F851vPJUCxmn6KPdY",
-  //   authDomain: "duantotnghiep-1f58b.firebaseapp.com",
-  //   projectId: "duantotnghiep-1f58b",
-  //   storageBucket: "duantotnghiep-1f58b.appspot.com",
-  //   messagingSenderId: "136015907416",
-  //   appId: "1:136015907416:web:ace5b87f9d0134a1ddd82d",
-  //   measurementId: "G-H3K932YQ7V"
-  // }
   firebaseConfig: {
-    apiKey: "AIzaSyDCzkz-d9elxiM884xPlf7kLyWU2xWguho",
-    authDomain: "postimage-duantn.firebaseapp.com",
-    databaseURL: "https://postimage-duantn-default-rtdb.firebaseio.com",
-    projectId: "postimage-duantn",
-    storageBucket: "postimage-duantn.appspot.com",
-    messagingSenderId: "1016967843615",
-    appId: "1:1016967843615:web:11a08928f0ad4a89156a2a",
-    measurementId: "G-K513PFWCJY"
+    apiKey: "AIzaSyBL8oFvS6G9d2dHC-F851vPJUCxmn6KPdY",
+    authDomain: "duantotnghiep-1f58b.firebaseapp.com",
+    projectId: "duantotnghiep-1f58b",
+    storageBucket: "duantotnghiep-1f58b.appspot.com",
+    messagingSenderId: "136015907416",
+    appId: "1:136015907416:web:ace5b87f9d0134a1ddd82d",
+    measurementId: "G-H3K932YQ7V"
   }
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyDCzkz-d9elxiM884xPlf7kLyWU2xWguho",
+  //   authDomain: "postimage-duantn.firebaseapp.com",
+  //   databaseURL: "https://postimage-duantn-default-rtdb.firebaseio.com",
+  //   projectId: "postimage-duantn",
+  //   storageBucket: "postimage-duantn.appspot.com",
+  //   messagingSenderId: "1016967843615",
+  //   appId: "1:1016967843615:web:11a08928f0ad4a89156a2a",
+  //   measurementId: "G-K513PFWCJY"
+  // }
 };
