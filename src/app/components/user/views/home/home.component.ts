80acb7110ceae8c72c125610f167c1560542d378
datvt2003 <tdat30404@gmail.com>
2023-12-06 05:36:46 +0700

    Revert "Merge branch 'Main-test' into dev-admin-dat"
    This reverts commit 275cbb9688c7b899acbb96e75f0922ad1c7e9325, reversing
    changes made to 4eb846795ed7078d1f9e7557fe064393b885a70a.

--- src/app/components/user/views/home/home.component.ts
+++ src/app/components/user/views/home/home.component.ts
@@ -7,51 +7,43 @@ import { HomeTour, SearchTour } from 'src/app/models/home.model';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit{
   public HomeTour!: HomeTour[];
   public currentPage = 1;
   public current = 1;
   public displayedTours: HomeTour[] = [];
   public toursPerPage = 6;
-  constructor(private HomeService: HomeService) {}
-
-  ngOnInit(): void {
-    console.log(this.HomeTour == null);
-    if (this.HomeTour == null) {
-      this.getAll();
-      this.autoChangeImage();
+  constructor(private HomeService: HomeService) {
   }
 
-  private getAll() {
-    this.HomeService.getAllHomeTour().subscribe((response) => {
+  ngOnInit(): void {
+      this.getAll();
+  }
+
+  private getAll(){
+    this.HomeService.getAllHomeTour().subscribe((response) =>{
       this.HomeTour = response;
-      this.displayedTours = this.getRandomTours(
-        this.HomeTour,
-        this.toursPerPage
-      );
+      this.displayedTours = this.getRandomTours(this.HomeTour, this.toursPerPage);
       console.log('Các tour: ', this.HomeTour);
       console.log('Tour ngau nhien: ', this.displayedTours);
     });
+}
+private getRandomTours(tours: HomeTour[], count: number): HomeTour[] {
+  // Shuffle the array to randomize the tours
+  let shuffledTours = tours.slice(); // Create a copy of the tours array
+  for (let i = shuffledTours.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledTours[i], shuffledTours[j]] = [shuffledTours[j], shuffledTours[i]];
   }
-  private getRandomTours(tours: HomeTour[], count: number): HomeTour[] {
-    // Shuffle the array to randomize the tours
-    let shuffledTours = tours.slice(); // Create a copy of the tours array
-    for (let i = shuffledTours.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledTours[i], shuffledTours[j]] = [
-        shuffledTours[j],
-        shuffledTours[i],
-      ];
-    }
 
-    // Return a slice of the shuffled array to get the specified number of random tours
-    return shuffledTours.slice(0, count);
-  }
-  formatCurrency(value: number): string {
-    const formattedValue = new Intl.NumberFormat('vi-VN', {
-      style: 'currency',
-      currency: 'VND',
-    }).format(value);
+  // Return a slice of the shuffled array to get the specified number of random tours
+  return shuffledTours.slice(0, count);
+}
+formatCurrency(value: number): string {
+  const formattedValue = new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  }).format(value);
 
   return formattedValue.replace('₫', '') + 'VNĐ';
 }
@@ -68,11 +60,5 @@ goToPreviousPage() {
   } else {
     this.current = this.HomeTour.length;
   }
-}
-
-autoChangeImage() {
-  setInterval(() => {
-    this.goToNextPage(); // Call the function to move to the next image
-  }, 2000); // Set the interval time (in milliseconds) - 5000ms (5 seconds) in this example
 }
 }
