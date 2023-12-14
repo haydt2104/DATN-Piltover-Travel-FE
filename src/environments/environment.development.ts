d37569a066dcf9370ea43b05dca922f0e0e6f64a
haydt2104 <144002142+haydt2104@users.noreply.github.com>
2023-12-14 13:19:17 +0700

    fix api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'http://piltover.us-east-1.elasticbeanstalk.com/',
   firebaseConfig: {
-    apiKey: "AIzaSyBL8oFvS6G9d2dHC-F851vPJUCxmn6KPdY",
-    authDomain: "duantotnghiep-1f58b.firebaseapp.com",
-    projectId: "duantotnghiep-1f58b",
-    storageBucket: "duantotnghiep-1f58b.appspot.com",
-    messagingSenderId: "136015907416",
-    appId: "1:136015907416:web:ace5b87f9d0134a1ddd82d",
-    measurementId: "G-H3K932YQ7V"
-  }
+    apiKey: 'AIzaSyBL8oFvS6G9d2dHC-F851vPJUCxmn6KPdY',
+    authDomain: 'duantotnghiep-1f58b.firebaseapp.com',
+    projectId: 'duantotnghiep-1f58b',
+    storageBucket: 'duantotnghiep-1f58b.appspot.com',
+    messagingSenderId: '136015907416',
+    appId: '1:136015907416:web:ace5b87f9d0134a1ddd82d',
+    measurementId: 'G-H3K932YQ7V',
+  },
   // firebaseConfig: {
   //   apiKey: "AIzaSyDCzkz-d9elxiM884xPlf7kLyWU2xWguho",
   //   authDomain: "postimage-duantn.firebaseapp.com",
